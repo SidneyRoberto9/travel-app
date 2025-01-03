7e098855f5b18f24e5f9adda346e173ef57b57f0
Sidney Roberto <sidneyrpsilva@gmail.om>
2025-01-03 13:16:37 -0300

    🏗️ build: Add Footer

---
+++ src/components/footer-column.tsx
@@ -0,0 +1,15 @@
+import { PropsWithChildren } from "react"
+
+interface FooterColumnProps extends PropsWithChildren {
+  title: string
+}
+const FooterColumn = ({ title, children }: FooterColumnProps) => {
+  return (
+    <div className="flex flex-col gap-5">
+      <h4 className="bold-18 whitespace-nowrap">{title}</h4>
+      {children}
+    </div>
+  )
+}
+
+export default FooterColumn
