282e87a6ce467f1a36596406ad4ebe5be15d794b
Sidney Roberto <sidneyrpsilva@gmail.om>
2025-01-03 01:22:32 -0300

    ✨ feat: Add components for section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+import { Fragment } from "react"
+
+import Camp from "@/components/camp"
+import Features from "@/components/features"
+import GetApp from "@/components/get-app"
+import Guide from "@/components/guide"
+import Hero from "@/components/hero"
+
 export default function Page() {
-  return <div>hello world</div>
+  return (
+    <Fragment>
+      <Hero />
+      <Camp />
+      <Guide />
+      <Features />
+      <GetApp />
+    </Fragment>
+  )
 }
---
+++ src/components/hero.tsx
@@ -0,0 +1,63 @@
+import Image from "next/image"
+
+import Button from "@/components/button"
+
+const Hero = () => {
+  return (
+    <section className="max-container padding-container flex flex-col gap-20 border-2 border-red-500 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
+      <div className="hero-map" />
+
+      <div className="flex-0 relative z-20 flex flex-col xl:w-1/2">
+        <Image
+          src="/camp.svg"
+          alt="camp"
+          width={50}
+          height={50}
+          className="absolute left-[-0.3125rem] top-[-1.875rem] w-10 lg:w-[3.125rem]"
+        />
+
+        <h1 className="bold-52 lg:bold-88">Putuk Truno Camp Area</h1>
+        <p className="regular-16 mt-6 text-gray-30 xl:max-w-[32.5rem]">
+          We want to be on each og your journeys seeking the satisfaction of
+          seeing the incorruptible beauty of nature. Wwe can help you on an
+          adventure around the world in just one app.
+        </p>
+
+        <div className="my-11 flex flex-wrap gap-5">
+          <div className="flex items-center gap-2">
+            {Array(5)
+              .fill(1)
+              .map((_, index) => (
+                <Image
+                  src="/star.svg"
+                  key={index}
+                  alt="star"
+                  width={24}
+                  height={24}
+                />
+              ))}
+          </div>
+
+          <p className="bold-16 lg:bold-20 text-blue-70">
+            198k
+            <span className="regular-16 lg:regular-20 ml-1">
+              Excellent Reviews
+            </span>
+          </p>
+        </div>
+
+        <div className="flex w-full gap-3 sm:flex-row">
+          <Button type="button" title="Download App" variant="btn_green" />
+          <Button
+            type="button"
+            title="How we work?"
+            icon="/play.svg"
+            variant="btn_white_text"
+          />
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
